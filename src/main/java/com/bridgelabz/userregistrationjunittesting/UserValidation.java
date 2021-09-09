3af3aa9d9e4da842cd7f26e2558bc16e30fdc7d8
namancg <naman.c@perfios.com>
2021-09-09 12:34:40 +0530

    ADDED: method to validate emailId

--- src/main/java/com/bridgelabz/userregistrationjunittesting/UserValidation.java
+++ src/main/java/com/bridgelabz/userregistrationjunittesting/UserValidation.java
@@ -6,5 +6,11 @@ public class UserValidation {
 		String nameRegex = "^[A-Z]{1}[a-z]{1,}";
 		return name.matches(nameRegex);
 	}
+	public boolean validateEmail(String email) {
+
+		String emailRegex = "^[a-zA-z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[\\.][a-z]{2,}(\\\\.[a-z]+)?";
+		return email.matches(emailRegex);
+	}
+	
 	
 }
